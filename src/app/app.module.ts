63f937e2aca262965c2e89ec71fc6b2caed96911
ugrasenanv <ugrasenanv@gmail.com>
2022-01-25 00:26:02 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
-import { AppComponent } from './app.component';
+// import { AppComponent } from './app.component';
 import { MatTableModule } from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableExporterModule } from 'mat-table-exporter';
