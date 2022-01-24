cc13a1e42a02f289f2d36a60a3ca21d5f603c4d7
ugrasenanv <ugrasenanv@gmail.com>
2022-01-25 00:03:25 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component, VERSION } from '@angular/core';
-
+export interface DataOfFootballers {
+  playerName: string;
+  playerCountry: string;
+  playerClub: string;
+}
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent extends CdkTableExporterModule {
   name = 'Angular ' + VERSION.major;
+
+  title = 'export-table-data-to-any-format';
+  dataSource: DataOfFootballers[] = [
+    {
+      playerName: 'Cristiano Ronaldo',
+      playerCountry: 'Pourtgal',
+      playerClub: 'Juventus',
+    },
+    {
+      playerName: 'Lionel Messi',
+      playerCountry: 'Argentina',
+      playerClub: 'Barcelona',
+    },
+    {
+      playerName: 'Neymar Junior',
+      playerCountry: 'Brazil',
+      playerClub: 'PSG',
+    },
+    {
+      playerName: 'Tonni Kroos',
+      playerCountry: 'Germany',
+      playerClub: 'Real Madrid',
+    },
+    {
+      playerName: 'Paul Pogba',
+      playerCountry: 'France',
+      playerClub: 'Manchester United',
+    },
+  ];
+  displayedColumns: string[] = ['playerName', 'playerCountry', 'playerClub'];
 }
