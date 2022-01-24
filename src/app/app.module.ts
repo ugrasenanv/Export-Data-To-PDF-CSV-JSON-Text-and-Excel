eaf276d7da0e7cbe7e30c97b0b896349088938b3
ugrasenanv <ugrasenanv@gmail.com>
2022-01-25 00:22:51 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,21 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { AppComponent } from './app.component';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableExporterModule } from 'mat-table-exporter';
+
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  // imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatTableModule,
+    BrowserAnimationsModule,
+    MatTableExporterModule,
+  ],
+
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
 })
