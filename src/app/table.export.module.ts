a1e86b4d18996f9ca68bc1d80c29bf27763b56a2
ugrasenanv <ugrasenanv@gmail.com>
2022-01-25 00:08:28 +0530

    1 file modified, 1 added

---
+++ src/app/table.export.module.ts
@@ -0,0 +1,9 @@
+import { CdkTableModule } from '@angular/cdk/table';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [],
+  imports: [CdkTableModule],
+  exports: [],
+})
+export class CdkTableExporterModule {}
